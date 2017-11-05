96fcfc2688ba102aa9e782d5096357190d7237c6
VeeShostak <VeeShostak@users.noreply.github.com>
2017-11-05 13:14:24 -0500

    add more  algorithms

--- algorithms-datastructures-1/main.cpp
+++ algorithms-datastructures-1/main.cpp
@@ -1,15 +1,20 @@
-//
-//  main.cpp
-//  algorithms-datastructures-1
-//
-//  Created by Vlad Shostak on 11/4/17.
-//  Copyright © 2017 Vlad Shostak. All rights reserved.
-//
 
+
+#include <stdio.h>
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+
+
+int main()
+{
+
+  
+    
+    
+    
+    
+    
 }
+
