0b0a154a1f4c53ea7c6296379566467340ef2384
VeeShostak <VeeShostak@users.noreply.github.com>
2017-11-05 13:13:51 -0500

    initial. todo: do solution

---
+++ algorithms-datastructures-1/algorithms-3/MapColoringProblem.cpp
@@ -0,0 +1,14 @@
+//
+//  MapColoringProblem.cpp
+//  algorithms-datastructures-1
+//
+//  Created by Vlad Shostak on 11/5/17.
+//  Copyright © 2017 Vlad Shostak. All rights reserved.
+//
+
+#include <stdio.h>
+
+
+// http://www.geeksforgeeks.org/backttracking-set-5-m-coloring-problem/
+
+
