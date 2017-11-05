d707825909b0e86351fc8a4961bc9c221201b176
VeeShostak <VeeShostak@users.noreply.github.com>
2017-11-04 23:52:36 -0400

    Initial Commit

---
+++ algorithms-datastructures-1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  algorithms-datastructures-1
+//
+//  Created by Vlad Shostak on 11/4/17.
+//  Copyright © 2017 Vlad Shostak. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
